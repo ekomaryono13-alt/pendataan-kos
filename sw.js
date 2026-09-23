b5f29aa6d9b11b629297d3c59072db501bad7772
ekomaryono13-alt <ekomaryono13@gmail.com>
2026-09-23 08:53:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,26 @@
-const CACHE='pendataan-kos-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="pendataan-kos-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest"];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
